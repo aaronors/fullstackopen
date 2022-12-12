f7446d3ec5a3eea9829e21473a5921331ee2c304
Aaron Salazar <aaronors@outlook.com>
2022-12-11 19:42:10 -0800

    part2 - ex2.6 - add name submission to phonebook + spacing

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState(""); // controlls for input element
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-}
+    const addPerson = (event) => {
+        event.preventDefault();
+        const personObject = {name: newName};
+        setPersons(persons.concat(personObject));
+        setNewName("");
+    };
 
-export default App
+    const handleNameChange = (event) => {
+        setNewName(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>debug: {newName}</div>
+        </div>
+    );
+};
+
+export default App;
