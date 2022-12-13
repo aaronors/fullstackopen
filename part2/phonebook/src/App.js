99831f923be398c8e0d7b1659b3f276e399b7fed
Aaron Salazar <aaronors@outlook.com>
2022-12-12 22:03:27 -0800

    part2 - ex2.11 - pull list of persons from json db
    - add db
    - add dependencies

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import "./App.css";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567"}]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [filter, setNewFilter] = useState("");
+
+    const hook = () => {
+        axios.get("http://localhost:3001/persons").then((response) => {
+            setPersons(response.data);
+        });
+    };
+
+    useEffect(hook, []);
 
     const filteredEntries = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
 
