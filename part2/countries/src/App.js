501ef45d8a69f7b5b9d0b9639a68c88d19fd16cb
Aaron Salazar <aaronors@outlook.com>
2022-12-12 23:27:19 -0800

    part2 - ex2.12 - add hooks + axios

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+    const [filter, setNewFilter] = useState("");
+    const [allCountries, setAllCountries] = useState([]);
 
+    const hook = () => {
+        axios.get("https://restcountries.com/v3.1/all").then((response) => {
+            setAllCountries(response.data);
+        });
+    };
+
+    useEffect(hook, []);
+
+    const filteredEntries = allCountries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()));
+
+    const handleFilterChange = (event) =>{
+        setNewFilter(event.target.value);
+    }    
+
+    return(
+        <>
+            <div>
+                find countries <input value={filter} onChange={handleFilterChange} />
+            </div>
+            <ul>
+                {filteredEntries.map((country) => (<li key={country.name.common}> {country.name.common} </li>))}
+            </ul>
+        </>
+    );
 }
 
 export default App;
+// over 10 countries returned -> show "Too many matches, specify another filter"
+// 10 or fewer countries -> show countries 
+// only one country matching query -> show basic data 
+
+
+//what to do
+// 1. hook up react app so that it retrieves and shows countries
+// 2. implement conditional rendering if too many matches
+// 3. implement coniditonal rendering if one country
+
