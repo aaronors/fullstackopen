71095be09d3dd06b0f883e27e209e228cf3112ad
Aaron Salazar <aaronors@outlook.com>
2022-12-20 18:59:13 -0800

    part4 - ex4.1 - make initial app work

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,43 +1,49 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
+console.log("connecting to", mongoUrl);
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
+mongoose
+    .connect(mongoUrl)
+    .then((result) => {
+        console.log("connected to MongoDB");
     })
-})
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message);
+    });
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(cors());
+app.use(express.json());
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.get("/api/blogs", (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
 
-const PORT = 3003
+app.post("/api/blogs", (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
