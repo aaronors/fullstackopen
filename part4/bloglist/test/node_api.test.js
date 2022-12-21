c2faff443a763f25754ef7fe2f48f2a5c2e19d02
Aaron Salazar <aaronors@outlook.com>
2022-12-21 01:07:38 -0800

    part4 - ex4.9 - test to check id exists on blogs

--- part4/bloglist/test/node_api.test.js
+++ part4/bloglist/test/node_api.test.js
@@ -14,26 +14,33 @@ beforeEach(async () => {
     await Promise.all(promiseArray);
 });
 
-test("blogs are returned as json", async () => {
-    await api
-        .get("/api/blogs")
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
-});
+// test("blogs are returned as json", async () => {
+//     await api
+//         .get("/api/blogs")
+//         .expect(200)
+//         .expect("Content-Type", /application\/json/);
+// });
 
-test("all blogs are returned", async () => {
+test("the id property on a blog must be set", async () => {
     const response = await api.get("/api/blogs");
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length);
+    const contents = response.body.map((r) => r._id);
+    expect(contents).toBeDefined();
 });
 
-test("a specific blog is within the returned blogs", async () => {
-    const response = await api.get("/api/blogs");
+// test("all blogs are returned", async () => {
+//     const response = await api.get("/api/blogs");
 
-    const contents = response.body.map((r) => r.title);
+//     expect(response.body).toHaveLength(helper.initialBlogs.length);
+// });
 
-    expect(contents).toContain("test2");
-});
+// test("a specific blog is within the returned blogs", async () => {
+//     const response = await api.get("/api/blogs");
+
+//     const contents = response.body.map((r) => r.title);
+
+//     expect(contents).toContain("test2");
+// });
 
 afterAll(() => {
     mongoose.connection.close();
