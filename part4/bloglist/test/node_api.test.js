5e291fc3f34073c974cad4970066f662d5d2a9a8
Aaron Salazar <aaronors@outlook.com>
2022-12-21 01:18:41 -0800

    part4 - ex4.10 - api post test + async/await call

--- part4/bloglist/test/node_api.test.js
+++ part4/bloglist/test/node_api.test.js
@@ -14,12 +14,12 @@ beforeEach(async () => {
     await Promise.all(promiseArray);
 });
 
-// test("blogs are returned as json", async () => {
-//     await api
-//         .get("/api/blogs")
-//         .expect(200)
-//         .expect("Content-Type", /application\/json/);
-// });
+test("blogs are returned as json", async () => {
+    await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+});
 
 test("the id property on a blog must be set", async () => {
     const response = await api.get("/api/blogs");
@@ -28,19 +28,40 @@ test("the id property on a blog must be set", async () => {
     expect(contents).toBeDefined();
 });
 
-// test("all blogs are returned", async () => {
-//     const response = await api.get("/api/blogs");
+test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
 
-//     expect(response.body).toHaveLength(helper.initialBlogs.length);
-// });
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
 
-// test("a specific blog is within the returned blogs", async () => {
-//     const response = await api.get("/api/blogs");
+test("a specific blog is within the returned blogs", async () => {
+    const response = await api.get("/api/blogs");
 
-//     const contents = response.body.map((r) => r.title);
+    const contents = response.body.map((r) => r.title);
 
-//     expect(contents).toContain("test2");
-// });
+    expect(contents).toContain("test2");
+});
+
+test("a valid blog can be added", async () => {
+    const newBlog = {
+        title: "newly inserted blog",
+        author: "test",
+        url: "test.com",
+        likes: 5
+    };
+
+    await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const insertedBlogs = await helper.blogsInDb();
+    expect(insertedBlogs).toHaveLength(helper.initialBlogs.length + 1);
+
+    const contents = insertedBlogs.map((b) => b.title);
+    expect(contents).toContain("newly inserted blog");
+});
 
 afterAll(() => {
     mongoose.connection.close();
