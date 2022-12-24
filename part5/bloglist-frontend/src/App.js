ff35f457b1003abdae2cb51dfe882104d5f093bf
Aaron Salazar <aaronors@outlook.com>
2022-12-24 02:01:22 -0800

    part5 - ex5.2 - log out functionality

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -46,6 +46,13 @@ const App = (props) => {
         }
     };
 
+    const handleLogout = () => {
+        window.localStorage.removeItem('loggedBlogAppUser');
+        setUser(null);
+        setUsername("");
+        setPassword("");
+    };
+
     const loginForm = () => (
         <div>
             <h2>Log in to application</h2>
@@ -76,7 +83,9 @@ const App = (props) => {
     const noteForm = () => (
         <div>
             <h2>blogs</h2>
-            <p>{user.name} logged-in</p>
+            <p>
+                {user.name} logged-in <button type="submit" onClick={handleLogout}>logout</button>
+            </p>
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
             ))}
