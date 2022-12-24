932e5594572af5463a8101e8cbe62665a2bfa959
Aaron Salazar <aaronors@outlook.com>
2022-12-23 20:07:56 -0800

    part4 - ex4.15 - http post for users

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,9 +1,10 @@
 const config = require("./utils/config");
 const express = require("express");
-require('express-async-errors');
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -22,5 +23,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
