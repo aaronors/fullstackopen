52cb25c529481df5e616ed3d7e0aeda379276f72
Aaron Salazar <aaronors@outlook.com>
2022-12-29 04:29:54 -0800

    part6 - ex6.19 - copy of app for connect mod

---
+++ part6/redux-anecdotes-connect/src/components/AnecdoteList.js
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addVote } from "../reducers/anecdoteReducer";
+import { createNotification, removeNotification } from "../reducers/notificationReducer";
+
+const AnecdoteList = () => {
+    const anecdotes = useSelector(({ anecdotes, filter }) => {
+        if (filter === "") {
+            return anecdotes;
+        }
+
+        return anecdotes.filter(a => a.content.includes(filter));
+
+    });
+
+    const dispatch = useDispatch();
+
+    const vote = (id) => {
+        dispatch(addVote(id));
+        dispatch(createNotification("A user has voted"));
+        setTimeout(() => {dispatch(removeNotification())}, 5000);
+    };
+    return (
+        <>
+            {anecdotes.map((anecdote) => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default AnecdoteList;
---
+++ part6/redux-anecdotes-connect/src/reducers/notificationReducer.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = null;
+
+const notificationSlice = createSlice({
+    name: "notification",
+    initialState,
+    reducers: {
+        createNotification(state, action) {
+            return action.payload;
+        },
+        removeNotification(state) {
+            return null;
+        },
+    },
+});
+
+export const { createNotification, removeNotification } = notificationSlice.actions;
+export default notificationSlice.reducer;
+
