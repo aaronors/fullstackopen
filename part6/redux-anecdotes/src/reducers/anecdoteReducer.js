733c408e12cd4cfcbcf51127ea7aedec7b2793eb
Aaron Salazar <aaronors@outlook.com>
2022-12-29 00:28:13 -0800

    part6 - ex6.6 - action creators

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,9 +20,6 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-    console.log("state now: ", state);
-    console.log("action", action);
-
     switch (action.type) {
         case "ADD_VOTE":
             const id = action.data.id;
@@ -39,7 +36,6 @@ const reducer = (state = initialState, action) => {
         default:
             return state;
     }
-
 };
 
 export const addVote = (id) => {
