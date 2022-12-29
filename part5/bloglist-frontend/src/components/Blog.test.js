bcbcf79af4839dbedeec9ce3e6208de2fce54b12
Aaron Salazar <aaronors@outlook.com>
2022-12-28 17:42:46 -0800

    part5 - ex5.14 - test blog shows all info after clicking show button

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,8 +4,11 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("<Blog/> renders content", () => {
-    const blog = {
+let container;
+let blog;
+
+beforeEach(() => {
+    blog = {
         _id: "1",
         title: "title",
         author: "author",
@@ -17,13 +20,26 @@ test("<Blog/> renders content", () => {
         },
         __v: 1
     };
+    
+    container = render(<Blog key={blog.id} blog={blog} updateBlogLikes={jest.fn()} deleteBlog={jest.fn()}/>).container;
+});
 
-    const container = render(<Blog key={blog.id} blog={blog} updateBlogLikes={jest.fn()} deleteBlog={jest.fn()}/>).container;
 
+test("<Blog/> renders content", () => {
     const div = container.querySelector(".blogHeader");
     expect(div).toHaveTextContent("title author")
 
     expect(div).not.toHaveTextContent(blog.url);
     expect(div).not.toHaveTextContent(blog.likes);
+
+});
+
+test("<Blog/> shows all info after clicking the show button", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("show");
+    await user.click(button);
+
+    const div = container.querySelector(".blogBody");
+    expect(div).not.toHaveStyle("display: none");
 
 });
