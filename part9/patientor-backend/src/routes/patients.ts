589861bf0a773d042ef29510268bc25a85274499
Aaron Salazar <aaronors@outlook.com>
2023-01-06 01:05:21 -0800

    part9 - ex9.12 - initial http post for patients

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import patientService from "../services/patientServices";
 
@@ -5,6 +6,18 @@ const patientRouter = express.Router();
 
 patientRouter.get("/", (_req, res) => {
     res.send(patientService.getEntries());
+});
+
+patientRouter.post("/", (req, res) => {
+    const { name, ssn, dateOfBirth, occupation, gender } = req.body;
+    const newPatient = patientService.addPatient({
+        name,
+        ssn,
+        dateOfBirth,
+        occupation,
+        gender,
+    });
+    res.json(newPatient);
 });
 
 export default patientRouter;
--- part9/patientor-backend/src/services/patientServices.ts
+++ part9/patientor-backend/src/services/patientServices.ts
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import patientData from "../../data/patients.json";
-import { NonSensitivePatient } from "../types";
+import { Patient, NewPatient, NonSensitivePatient } from "../types";
+import { v1 as uuid } from "uuid";
 
-const patients: Array<NonSensitivePatient> = patientData as Array<NonSensitivePatient> ;
+const patients: Array<NonSensitivePatient> =
+    patientData as Array<NonSensitivePatient>;
 
 const getEntries = (): NonSensitivePatient[] => {
     return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -9,8 +13,18 @@ const getEntries = (): NonSensitivePatient[] => {
         name,
         dateOfBirth,
         gender,
-        occupation
+        occupation,
     }));
 };
 
-export default { getEntries };
+const addPatient = (entry: NewPatient): Patient => {
+    const newPatient = {
+        id: uuid(),
+        ...entry,
+    };
+
+    patientData.push(newPatient);
+    return newPatient;
+};
+
+export default { getEntries, addPatient };
