7b19c064d503132b8112f1eeecffa2dffd976697
Aaron Salazar <aaronors@outlook.com>
2023-01-10 21:08:59 -0800

    part9 - ex9.21 - formatting state

--- part9/patientor/src/state/state.tsx
+++ part9/patientor/src/state/state.tsx
@@ -4,55 +4,52 @@ import { Patient } from "../types";
 import { Action } from "./reducer";
 
 export type State = {
-  patients: { [id: string]: Patient };
-  currentPatient: Patient;
+    patients: { [id: string]: Patient };
+    currentPatient: Patient;
 };
 
 const initialState: State = {
-  patients: {},
-  currentPatient: {} as Patient,
+    patients: {},
+    currentPatient: {} as Patient,
 };
 
 export const StateContext = createContext<[State, React.Dispatch<Action>]>([
-  initialState,
-  () => initialState
+    initialState,
+    () => initialState,
 ]);
 
 type StateProviderProps = {
-  reducer: React.Reducer<State, Action>;
-  children: React.ReactElement;
+    reducer: React.Reducer<State, Action>;
+    children: React.ReactElement;
 };
 
-export const StateProvider = ({
-  reducer,
-  children
-}: StateProviderProps) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <StateContext.Provider value={[state, dispatch]}>
-      {children}
-    </StateContext.Provider>
-  );
+export const StateProvider = ({ reducer, children }: StateProviderProps) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <StateContext.Provider value={[state, dispatch]}>
+            {children}
+        </StateContext.Provider>
+    );
 };
 export const useStateValue = () => useContext(StateContext);
 
 export const setPatientList = (patientList: Patient[]): Action => {
     return {
         type: "SET_PATIENT_LIST",
-        payload: patientList
+        payload: patientList,
     };
 };
 
 export const addPatient = (newPatient: Patient): Action => {
-    return { 
-        type: "ADD_PATIENT", 
-        payload: newPatient 
+    return {
+        type: "ADD_PATIENT",
+        payload: newPatient,
     };
 };
 
 export const setCurrentPatient = (currentPatientData: Patient): Action => {
-    return { 
-        type: "SET_CURRENT_PATIENT", 
-        payload: currentPatientData 
-    };    
+    return {
+        type: "SET_CURRENT_PATIENT",
+        payload: currentPatientData,
+    };
 };
